a4c5266c60c1a9227beead672a1332823fd814a9
Caleb Stewart <caleb.c.stewart@proton.me>
2025-01-21 14:35:13 -0600

    add basic repl functionality

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func cleanInput(text string) []string {
-	return strings.Split(strings.TrimSpace(text), " ")
+	return strings.Fields(strings.ToLower(text))
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	prompt := "Pokedex > "
+	for {
+		fmt.Print(prompt)
+		scanner.Scan()
+		text := scanner.Text()
+		cleaned_text := cleanInput(text)
+		fmt.Printf("Your command was: %s\n", cleaned_text[0])
+	}
 }
